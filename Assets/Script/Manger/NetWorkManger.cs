59f5f6f6a38af9a7d1da82757f403444474c99c1
WangNan <435629456@qq.com>
2018-12-25 17:47:53 +0800

    no message

---
+++ Assets/Script/Manger/NetWorkManger.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using GameFramework.Network;
+using UnityGameFramework.Runtime;
+
+public class NetWorkManger : Singleton<NetWorkManger> {
+    private  NetworkComponent _instance;
+    public  NetworkComponent _Instance
+    {
+        get
+        {
+            if(_instance==null)
+            {
+                _instance = GameEntry.GetComponent<NetworkComponent>();
+            }
+            return _instance;
+        }
+    }
+
+
+}
